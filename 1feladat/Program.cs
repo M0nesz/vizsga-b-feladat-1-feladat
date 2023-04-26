88326fd8718d4682f4d364ce06fbc8ce880fabde
Monesz <90346447+M0nesz@users.noreply.github.com>
2023-04-26 11:03:13 +0200

    Add files via upload

---
+++ 1feladat/Program.cs
@@ -0,0 +1,115 @@
+﻿//1.feladat 
+
+using _1feladat;
+
+List<TEXT> text = new List<TEXT>();
+string[] txt = File.ReadAllLines("nyeremenyek.txt");
+foreach (var item in txt)
+{
+    string[] values = item.Split(';');
+    TEXT items_object = new TEXT(int.Parse(values[0]), values[1], int.Parse(values[2]), values[3], values[4]);
+    text.Add(items_object);
+}
+
+/*foreach (var item in text)
+{
+    Console.WriteLine($"{item.id} {item.name} price: {item.price} times: {item.times} nyertes: {item.nyertese}");
+}*/
+
+Console.WriteLine("2. feladat");
+Console.WriteLine("Adj mege gy ID-t");
+
+string input_id = Console.ReadLine();
+int int_inputid = int.Parse(input_id);
+
+foreach (var item in text)
+{
+    if (item.id == int_inputid)
+    {
+        Console.WriteLine($"Sorszám: {item.id}, Név: {item.name}, nyeremény: {item.price}, alkalom: {item.times}, nyert-e: {item.nyertese}");
+    }
+}
+Console.WriteLine("3.feladat");
+
+string mostFrequentName = null;
+int maxCount = 0;
+int sum = 0;
+
+foreach (var item in text)
+{
+    int count = 0;
+    int priceSum = 0;
+
+    foreach (var otherItem in text)
+    {
+        if (otherItem.name == item.name)
+        {
+            count++;
+            priceSum += otherItem.price;
+        }
+    }
+
+    if (count > maxCount)
+    {
+        maxCount = count;
+        mostFrequentName = item.name;
+        sum = priceSum;
+    }
+}
+
+Console.WriteLine($"A legtöbb nyereményt {mostFrequentName} névre nyerték, összesen {maxCount} alkalommal, a nyertes össznyereménye {sum} forint.");
+
+Console.WriteLine("4. feladat");
+int nyertdb = 0;
+List<int> nyertesek = new List<int>();
+foreach (var item in text)
+{
+    if (item.nyertese == "nyertes")
+    {
+        nyertdb++;
+        nyertesek.Add(item.id);
+    }
+}
+Console.WriteLine($"Ennyi játszma lett nyerve: {nyertdb} ");
+Console.WriteLine("5. feladat");
+
+TEXT lowestPriceItem = null;
+
+foreach (var item in text)
+{
+    if (lowestPriceItem == null || item.price < lowestPriceItem.price)
+    {
+        lowestPriceItem = item;
+    }
+}
+
+Console.WriteLine($"A legkisebb nyereményt {lowestPriceItem.name} névvel rendelkező személy nyerte, a nyeremény összege: {lowestPriceItem.price} forint.");
+
+Console.WriteLine("6. feladat");
+
+var namesStartingWithA = text
+    .Where(item => item.name.StartsWith("a"))
+    .Select(item => item.name)
+    .Take(5)
+    .ToList();
+
+Console.WriteLine("Az 'a'-val kezdődő nevek:");
+foreach (var name in namesStartingWithA)
+{
+    Console.WriteLine(name);
+}
+
+foreach (var item in text)
+{
+    foreach (var item2 in nyertesek)
+    {
+        if (item.id == item2)
+        {
+            Console.WriteLine($"Nyertes neve: {item.name}, Sorszáma: {item.id}, nyeremény: { item.price}");
+        }
+    }
+}
+
+
+
+
---
+++ 1feladat/TEXT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1feladat
+{
+    class TEXT
+    {
+        public int id;
+        public string name;
+        public int price;
+        public string times;
+        public string nyertese;
+
+        public TEXT(int id, string name, int price, string times, string nyertese)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.times = times;
+            this.nyertese = nyertese;
+        }
+    }
+}
